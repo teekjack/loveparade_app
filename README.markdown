195c73cc3e52465431203c5a647c92007a748a6c
Pratik Mukerji <pratik@pratikmukerji.com>
2011-03-01 21:55:24 +0100

    test

---
+++ README.markdown
@@ -0,0 +1,2 @@
+App to tag the location of a Youtube video. For Crowd disaster observation organization.
+
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+		@title = "Love Parade YouTube Videos"
+  end
+
+  def contact
+		@title = "Contact"
+  end
+
+end
