a31b14ca1119d12da674821fad0f7792b4823a60
Pratik Mukerji <pratik@pratikmukerji.com>
2011-03-01 18:53:32 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoveparadeApp::Application.config.secret_token = '4398722ecbfab696f40577a5566bccdc79ddce21c1b52e3ad01496415f967b15023535fa0786b89e6f9ae36e8f937de1778d702eb1351cd2eb97a86ef2962b38'
