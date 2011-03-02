c10012c6d4f319ea730e47aa11de6a1a0f724ecd
Pratik Mukerji <pratik@pratikmukerji.com>
2011-03-02 16:02:42 +0100

    added videos to db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+	USER_NAME, PASSWORD = "loveparade", "1234"
+	before_filter :authenticate
+
+	private
+ 	  def authenticate
+ 	    authenticate_or_request_with_http_basic do |user_name, password|
+ 	      user_name == USER_NAME && password == PASSWORD
+ 	  end
+ 	end
 end
