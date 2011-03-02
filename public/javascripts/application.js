e35ea0cd0ead4a32a9736b593451b63030b3fbb4
Pratik Mukerji <pratik@pratikmukerji.com>
2011-03-02 16:51:01 +0100

    test

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -59,8 +59,31 @@ function initializemaps(){
     mapTypeId: google.maps.MapTypeId.SATELLITE
   };
 	map = new google.maps.Map(document.getElementById("map_canvas"), options);
+
+					var image = new google.maps.MarkerImage(
+	  			'/images/marker-images/image.png',
+	  			new google.maps.Size(20,21),
+	  			new google.maps.Point(0,0),
+	  			new google.maps.Point(10,21)
+				);
+
+				var shadow = new google.maps.MarkerImage(
+	  			'/images/marker-images/shadow.png',
+	  			new google.maps.Size(34,21),
+	  			new google.maps.Point(0,0),
+	  			new google.maps.Point(10,21)
+				);
+
+				var shape = {
+	  			coord: [17,0,17,1,17,2,18,3,19,4,19,5,19,6,19,7,19,8,19,9,19,10,19,11,18,12,18,13,18,14,18,15,18,16,18,17,18,18,18,19,17,20,3,20,3,19,3,18,3,17,3,16,3,15,1,14,1,13,1,12,0,11,0,10,0,9,0,8,0,7,0,6,1,5,1,4,4,3,3,2,3,1,4,0,17,0],
+ 	 				type: 'poly'
+				};
+  
 	var marker = new google.maps.Marker({
-    position: latlng,
+		icon: image,
+  	shadow: shadow,
+  	shape: shape,    
+		position: latlng,
     title: 'Point A',
     map: map,
     draggable: true
@@ -92,30 +115,6 @@ function initializemaps(){
   //GEOCODER
   geocoder = new google.maps.Geocoder();
   
-				var image = new google.maps.MarkerImage(
-	  			'/images/marker-images/image.png',
-	  			new google.maps.Size(20,21),
-	  			new google.maps.Point(0,0),
-	  			new google.maps.Point(10,21)
-				);
 
-				var shadow = new google.maps.MarkerImage(
-	  			'/images/marker-images/shadow.png',
-	  			new google.maps.Size(34,21),
-	  			new google.maps.Point(0,0),
-	  			new google.maps.Point(10,21)
-				);
-
-				var shape = {
-	  			coord: [17,0,17,1,17,2,18,3,19,4,19,5,19,6,19,7,19,8,19,9,19,10,19,11,18,12,18,13,18,14,18,15,18,16,18,17,18,18,18,19,17,20,3,20,3,19,3,18,3,17,3,16,3,15,1,14,1,13,1,12,0,11,0,10,0,9,0,8,0,7,0,6,1,5,1,4,4,3,3,2,3,1,4,0,17,0],
- 	 				type: 'poly'
-				};
-  
-  marker = new google.maps.Marker({
-					icon: image,
-  				shadow: shadow,
-  				shape: shape,
-					map: map
-  });
 
 }
