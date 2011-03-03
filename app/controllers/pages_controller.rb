f288e8deff2810dd77e3a0cd2d9e4f4e0812022c
Pratik Mukerji <pratik@pratikmukerji.com>
2011-03-03 11:26:05 +0100

    test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,22 +9,27 @@ class PagesController < ApplicationController
 
 	def mapall
 		@title = "All Videos, one Map"
+		@locations = Video.find(:all)
 	end
 
 	def map1530
 		@title = "15:30-16:00"
+		@locations = Video.where(:time => "15:30-16:00")
 	end
 	
 	def map1601
 		@title = "16:01-16:30"
+		@locations = Video.where(:time => "16:01-16:30")
 	end
 	
 	def map1631
 		@title = "16:31-17:00"
+		@locations = Video.where(:time => "16:31-17:00")
 	end
 
 	def map1701
 		@title = "17:01-17:30"
+		@locations = Video.where(:time => "17:01-17:30")
 	end
 
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,17 +2,64 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
 		
-	initializemaps();
+	if ( $("#formpage").length ) {
+ 		initializemapform();
+	}
+	else if ($("#showpage").length){
+		initializemaps();
+	}
+	
               
 });
 
+var geocoder;
+var map;
+var marker;
+   
+function initializemaps(){
+//MAP
+  var latlng = new google.maps.LatLng(51.420466,6.772835);
+  var options = {
+    zoom: 17,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  };
+       
+  map = new google.maps.Map(document.getElementById("map_canvas"), options);
+
+		
+	var latlng = new google.maps.LatLng(51.420466,6.772835);
+     
+  //GEOCODER
+  geocoder = new google.maps.Geocoder();
   
+				var image = new google.maps.MarkerImage(
+	  			'/images/marker-images/image.png',
+	  			new google.maps.Size(20,21),
+	  			new google.maps.Point(0,0),
+	  			new google.maps.Point(10,21)
+				);
+
+				var shadow = new google.maps.MarkerImage(
+	  			'/images/marker-images/shadow.png',
+	  			new google.maps.Size(34,21),
+	  			new google.maps.Point(0,0),
+	  			new google.maps.Point(10,21)
+				);
+
+				var shape = {
+	  			coord: [17,0,17,1,17,2,18,3,19,4,19,5,19,6,19,7,19,8,19,9,19,10,19,11,18,12,18,13,18,14,18,15,18,16,18,17,18,18,18,19,17,20,3,20,3,19,3,18,3,17,3,16,3,15,1,14,1,13,1,12,0,11,0,10,0,9,0,8,0,7,0,6,1,5,1,4,4,3,3,2,3,1,4,0,17,0],
+ 	 				type: 'poly'
+				};
+	placemarker();
+
+}  
 
 var geocoder;
 var map;
 var marker;
 
-function initializemaps(){
+function initializemapform(){
 //MAP
   var latlng = new google.maps.LatLng(51.420466,6.772835);
   var options = {
@@ -63,15 +110,5 @@ function initializemaps(){
 		$("#video_latitude").val(pos.lat());
 		$("#video_longitude").val(pos.lng());
   });
-       
   
-
-		
-	var latlng = new google.maps.LatLng(51.420466,6.772835);
-     
-  //GEOCODER
-  geocoder = new google.maps.Geocoder();
-  
-
-
 }
