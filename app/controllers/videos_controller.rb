8f319197b2832ff3331146a5a70c7a7013acd32a
Pratik Mukerji <pratik@pratikmukerji.com>
2011-03-03 10:00:33 +0100

    added index of videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,9 +1,10 @@
 class VideosController < ApplicationController
-  def new
-		@timespans = ["","15:30-16:00","16:01-16:30","16:31-17:00","17:01-17:30"]
+	 
+	def new
 		@video = Video.new
 		@title = "Add new video"
-  end
+		@timespans = ["","15:30-16:00","16:01-16:30","16:31-17:00","17:01-17:30"]   
+	end
 
 	def create
     @video = Video.new(params[:video])
@@ -12,9 +13,32 @@ class VideosController < ApplicationController
       redirect_to @video
     else
       @title = "Add Video"
-			@timespans = ["","15:30-16:00","16:01-16:30","16:31-17:00","17:01-17:30"]
+			@timespans = ["","15:30-16:00","16:01-16:30","16:31-17:00","17:01-17:30"] 
       render 'new'
     end
+  end
+	
+	def edit
+    @video = Video.find(params[:id])
+    @title = "Edit Video"
+		@timespans = ["","15:30-16:00","16:01-16:30","16:31-17:00","17:01-17:30"] 
+  end
+
+	def update
+    @video = Video.find(params[:id])
+    if @video.update_attributes(params[:video])
+      flash[:success] = "Video updated."
+      redirect_to @video
+    else
+      @title = "Edit Video"
+			@timespans = ["","15:30-16:00","16:01-16:30","16:31-17:00","17:01-17:30"] 
+      render 'edit'
+    end
+  end
+
+	def index
+    @title = "All Videos"
+    @videos = Video.all
   end
 
 	def show
