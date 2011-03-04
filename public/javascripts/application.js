d2db9e46ed6f99a0669ebb2313401a03c229c6bd
Pratik Mukerji <pratik@pratikmukerji.com>
2011-03-04 10:31:03 +0100

    integer

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function(){
-		
+	
+	 $("a").click(function() {
+    link_host = this.href.split("/")[2];
+    document_host = document.location.href.split("/")[2];
+
+    if (link_host != document_host) {
+      window.open(this.href);
+      return false;
+    }
+  });
+
 	if ( $("#formpage").length ) {
  		initializemapform();
 	}
