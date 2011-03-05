f17cf0bd467838aebfe912fa65fd85dfa30a5b9a
Pratik Mukerji <pratik@pratikmukerji.com>
2011-03-05 17:59:12 +0100

    test

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -38,7 +38,7 @@ function initializemaps(){
   map = new google.maps.Map(document.getElementById("map_canvas"), options);
 
 		
-	var latlng = new google.maps.LatLng(51.420466,6.772805);
+	var latlng = new google.maps.LatLng(51.420466,6.772105);
      
   //GEOCODER
   geocoder = new google.maps.Geocoder();
